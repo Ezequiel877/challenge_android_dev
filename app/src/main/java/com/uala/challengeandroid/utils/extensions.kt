bd656ee393d01992217441209efc8e521c979eae
Yhon <yonatanezeq@gmail.com>
2025-07-16 21:01:59 -0300

    push 07/16

--- app/src/main/java/com/uala/challengeandroid/utils/extensions.kt
+++ app/src/main/java/com/uala/challengeandroid/utils/extensions.kt
@@ -103,7 +103,6 @@ sealed interface Result<out T> {
     data class Error(val exception: Throwable) : Result<Nothing>
     data object Loading : Result<Nothing>
 }
-
 fun <T> Result<T>.ifSuccess(block: (T) -> Unit) {
     if (this is Result.Success) block(data)
 }
